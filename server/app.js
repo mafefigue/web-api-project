4b9133dc684d4cb41c041b1ac0898af025c0710c
Jona40 <00014620@uca.edu.sv>
2023-11-20 19:30:12 -0600

    Inicializacion del proyecto

---
+++ server/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const app = express();
+
+//logger for request
+app.use(logger('dev'));
+
+//body parsers
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+//static router
+app.use(express.static(path.join(__dirname, 'public')));
+
+module.exports = app;
