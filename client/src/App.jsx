00dc89148d984ca4e5bc188a4a33d6fab4199539
mafefigue <00109822@uca.edu.sv>
2023-11-23 16:03:18 -0600

    estructura inicial y estilos de signIn, falta funcionalidad

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,22 @@
+// import { Route, BrowserRouter, Routes } from "react-router-dom";
+import SignIn from "./pages/signin"
+
+
+
 function App() {
 
   return (
-    <>
-
-    </>
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/signIn" element={<SignIn/>} />
+    //   </Routes>
+    // </BrowserRouter>
+    <SignIn/>
+    
+    
+  
   )
 }
 
 export default App
+
