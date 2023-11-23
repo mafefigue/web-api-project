fcc028748f424fb1102905fc196f5a3f4d6e3c89
Daniel Juarez <00012922@uca.edu.sv>
2023-11-22 21:14:41 -0600

    chore: update articule model

--- server/models/articulo.model.js
+++ server/models/articulo.model.js
@@ -7,11 +7,6 @@ const articuloShema = new Schema({
         required: true,
         trim: true
     },
-    precio:{
-        type: Number,
-        min: 0.00,
-        default: 0.00
-    },
     descripcion: {
         type: String,
         trim: true,
@@ -19,7 +14,13 @@ const articuloShema = new Schema({
     },
     lista_deseos: {
         type: String,
-        trim: true
+        trim: true,
+        required: true
+    },
+    precio:{
+        type: Number,
+        min: 0.00,
+        default: 0.00
     },
     hidden: {
         type: Boolean,
