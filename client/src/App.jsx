7c145c94db393c9935e3b45c584c29630b315a80
mafefigue <00109822@uca.edu.sv>
2023-11-23 20:31:55 -0600

    estructura inicial del perfil de usuario

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,27 @@
 function App() {
-
   return (
-    <>
+    <div>
+      <h1>Mi perfil</h1>
+      <div>
+        <h3>nombre usuario</h3>
+        <img></img>
+        <div>
+          <button></button>
+          <button></button>
+        </div>
+        <div>
+          <h2>información de contacto</h2>
 
-    </>
-  )
+        </div>
+        <div>
+          <h2>Mis publicaciones</h2>
+          <div>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
