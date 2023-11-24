4b11513796be9f15116a9aa8c841bc7e190b0d95
Daniel Juarez <00012922@uca.edu.sv>
2023-11-23 21:51:32 -0600

    feat: update auth controller and validator

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -5,7 +5,7 @@ const controller = {};
 controller.register = async(req, res, next)=>{
     try {
         const { username, email, password }= req.body;
-        const user = await User.findOne({ $or:[{username: username}, {email: email}] });
+        const user = await User.findOne({ $or:[{username: username}, {correo: email}] });
         if(user){
             return res.status(409).json({ error: "User already exists!"})
         }
@@ -16,6 +16,29 @@ controller.register = async(req, res, next)=>{
         });
         await newUser.save();
         return res.status(201).json({ message: "User registered" });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({error: "Internal Server Error"});
+    }
+}
+
+controller.updateUser = async(req, res, next)=>{
+    try {
+        const { id }= req.params;
+        const { username, password, picture} = req.body;
+        const user = await User.findById(id);
+        if(!user){
+            return res.status(404).json({ error: "User not found"})
+        };
+        const updatedUser = await User.findByIdAndUpdate( id, {
+            username: username,
+            contrasenia: password,
+            profile_pic: picture
+        }, {new: true});
+        if(!updatedUser){
+            return res.status(500).json({ error: "User not updated"})
+        }
+        return res.status(200).json({ message: "User updated", user: updatedUser });
     } catch (error) {
         console.error(error);
         return res.status(500).json({error: "Internal Server Error"});
--- server/validators/auth.validator.js
+++ server/validators/auth.validator.js
@@ -1,18 +1,43 @@
-const { body } = require("express-validator");
+const { body, param } = require("express-validator");
 
 const validators = {};
 const passwordRegexp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,32})/
 
 validators.registerValidator = [
     body("username")
+        .trim()
         .notEmpty().withMessage("User is required")
         .isLength( {min:4 , max:32 }).withMessage("Username format incorrect"),
     body("email")
+        .trim()
         .notEmpty().withMessage("User is required")
         .isEmail().withMessage("Email format incorrect"),
     body("password")
+        .trim()
         .notEmpty().withMessage("Password is required")
         .matches(passwordRegexp).withMessage("Password format incorrect")
+];
+
+validators.updateUserValidator = [
+    body("username")
+        .optional()
+        .trim()
+        .isLength( {min:4 , max:32 }).withMessage("Username format incorrect"),
+    body("password")
+        .optional()
+        .trim()
+        .matches(passwordRegexp).withMessage("Password format incorrect"),
+    body("picture")
+        .optional()
+        .trim()
+        .isString().withMessage("Picture is a string")
+        .isURL().withMessage("Picture is a URL")
+];
+
+validators.idInParams = [
+    param("id")
+        .notEmpty().withMessage("ID is required")
+        .isMongoId().withMessage("ID is MongoID")
 ];
 
 module.exports = validators;
