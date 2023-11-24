314950ddce44c6d48ff9f23f624586ce4446b630
mafefigue <00109822@uca.edu.sv>
2023-11-24 15:13:54 -0600

    pequeños cambios en el estilo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,10 @@
+import profile from './pages/profile'
+
 function App() {
   return (
-    <div>
-      <h1>Mi perfil</h1>
-      <div>
-        <h3>nombre usuario</h3>
-        <img></img>
-        <div>
-          <button></button>
-          <button></button>
-        </div>
-        <div>
-          <h2>información de contacto</h2>
-
-        </div>
-        <div>
-          <h2>Mis publicaciones</h2>
-          <div>
-            
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+   <profile/> 
+   
+  )
 }
 
 export default App;
---
+++ client/src/pages/profile.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function profile() {
+  return (
+    <div>
+      <h1>Mi perfil</h1>
+      <div>
+        <h3>nombre usuario</h3>
+        <img></img>
+        <div>
+          <button></button>
+          <button></button>
+        </div>
+        <div>
+          <h2>información de contacto</h2>
+        </div>
+        <div>
+          <h2>Mis publicaciones</h2>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default profile;
