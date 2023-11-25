b9cd22e69112c02747023796767ac964227ba141
mafefigue <00109822@uca.edu.sv>
2023-11-25 16:42:56 -0600

    vistas de perfil de usuario, y modificar perfil de usuario finalizadas

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,18 @@
 // import { Route, Router, Routes } from "react-router";
 // import { Route, BrowserRouter, Routes } from "react-router-dom";
 import Profile from './pages/profile'
+//import ModificarPerfil from './pages/modifProfile'
 
 function App() {
   return (
   //   <BrowserRouter>
   //   <Routes>
   //     <Route path="/perfil" element={<Profile />} />
+  //     <Route path="/modificarPerfil" element ={<ModificarPerfil />} /> 
   //   </Routes>
   // </BrowserRouter>
   <Profile/>
+  //<ModificarPerfil/>
   );
 }
 
--- client/src/components/PostList.jsx
+++ client/src/components/PostList.jsx
@@ -20,4 +20,25 @@
 //   );
 // };
 
+// function TaskList() {
+//     const { tasks } = useContext(TaskContext);
+  
+//     if (tasks.length === 0) {
+//       return (
+//         <h1 className="text-white text-4xl font-bold text-center">
+//           aún no hay tareas
+//           <AiOutlineSmile className="max-w-md mx-auto" />
+//         </h1>
+//       );
+//     }
+  
+//     return (
+//       <div className="grid grid-cols-4 gap-2 p-4 rounded-md">
+//         {tasks.map((task) => (
+//           <TaskCard key={task.id} task={task} /> 
+//         ))}
+//       </div>
+//     );
+//   }
+
 // export default PostList;
