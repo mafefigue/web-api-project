cc3da0292613b92eb4cb0db158ad746375dd3954
Jona40 <00014620@uca.edu.sv>
2023-11-24 19:18:42 -0600

    footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import { Footer } from "./components/Footer"
 import {Header} from "./components/Header"
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <>
       <div className="">
       <Header />
+      <Footer />
       </div>
     
     </>
