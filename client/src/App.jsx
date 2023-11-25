25f06cd5e5ba913e6c4510b2eb62a396cdd81af1
Jona40 <00014620@uca.edu.sv>
2023-11-25 12:59:09 -0600

    iniciando componente card

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
     <>
       <div className="">
-      <Header />
+      
       </div>
     
     </>
---
+++ client/src/components/CardProduct.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const CardProduct = () => {
+  return (
+    <div>CardProduct</div>
+  )
+}
