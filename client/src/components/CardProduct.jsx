5adb58faaa0d4fd7d2ab1275f20d943eac1ffddd
Jona40 <00014620@uca.edu.sv>
2023-11-28 12:47:43 -0600

    cardlist a probar

--- client/src/components/CardProduct.jsx
+++ client/src/components/CardProduct.jsx
@@ -1,27 +1,4 @@
-//ruta de donde sacariamos la info de las publicasiones
-import Logo from "../assets/Logo.svg"
 
-
-export const CardProduct = () => {
-  return (
-    <div className="max-w-sm rounded-3xl overflow-hidden shadow-lg cursor-pointer bg-slate-500">
-      <img className="w-full" src={Logo} alt="foto" />
-      <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">calculadora ti</div>
-        <p className=" text-white text-base">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-          Voluptatibus quia, nulla! Maiores et perferendis eaque, 
-          exercitationem praesentium nihil.
-        </p>
-      </div>
-      <div className="px-6 pt-4 pb-2">
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#estudios</span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#nuevo</span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#usado</span>
-      </div>
-    </div>
-  );
-};
 
 
 
